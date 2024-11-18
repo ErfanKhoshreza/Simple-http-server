20d75472321920fb6224e2259f3f7fdb9f8cbaf6
Erfan Khoshreza <erfanbusy@gmail.com>
2024-11-18 09:09:18 +0330

    created the http server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express.js HTTP Server!');
+});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
